af5639fb9f4aac9a65d6ea6a51b2002e7d733659
Matthew Cohen <matt.cohen182@gmail.com>
2018-12-05 15:28:28 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,22 @@
 class Pokemon
+  
+  @@all = []
+  attr_accessor :name, :id, :type, :db
+  
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+    @pokemons = []
+  end
+  
+  def self.save(name, type, db)
+    @pokemons 
+    
+  end
+    
 end
+
+
